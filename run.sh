fdad1a8bcca4284bcfc1b1f4e57bc7387fbd5e0a
Eric <Ljh781118@outlook.com>
2025-01-20 22:33:03 +0800

    feat(github-actions): update workflow to commit changes to output.yaml

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@
 # 使用 output.yaml 作为输出文件
 # 使用 normal 作为日志详细程度
 pip3 install requests pyyaml
-python3 helper.py sources.yaml output.yaml normal
+python3 helper.py sources.yaml output.yaml verbose
 echo "Clash 配置文件已生成：output.yaml"
