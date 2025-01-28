2d553977e9fb171d4f80472289698f080b2cf484
EricLeeaaaaa <ericleeaaaaa@github.com>
2025-01-28 17:46:54 +0800

    refactor(workflows): remove GitHub Actions workflow

--- commit.bat
+++ commit.bat
@@ -1,10 +1,15 @@
 @echo off
+chcp 65001
 
 REM 运行Python脚本生成配置文件
 python helper.py sources.yaml output.yaml verbose
 echo Clash配置文件已生成：output.yaml
 
 REM 配置Git并提交更改
+git config --global core.quotepath false
+git config --global i18n.commitencoding utf-8
+git config --global i18n.logoutputencoding utf-8
+git config --global gui.encoding utf-8
 git config --global user.name "EricLeeaaaaa"
 git config --global user.email "ericleeaaaaa@github.com"
 git add output.yaml
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 运行 helper.py 脚本
-# 使用 sources.yaml 作为输入文件
-# 使用 output.yaml 作为输出文件
-# 使用 normal 作为日志详细程度
-pip3 install requests pyyaml
-python3 helper.py sources.yaml output.yaml verbose
-echo "Clash 配置文件已生成：output.yaml"
